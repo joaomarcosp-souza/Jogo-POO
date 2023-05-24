64a176cf5bf499a29d273ce30044c6f52eb9d9d1
João Marcos <112454993+joaomarcosp-souza@users.noreply.github.com>
2023-05-23 22:44:28 -0300

    Upando imagem

---
+++ projeto/src/br/ifpr/paranavai/jogo/modelo/Fase.java
@@ -0,0 +1,13 @@
+package br.ifpr.paranavai.jogo.modelo;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+public class Fase {
+    private Image fundinho;
+
+    public Fase() {
+       ImageIcon carregando = new ImageIcon("", null);
+    }
+    
+}
