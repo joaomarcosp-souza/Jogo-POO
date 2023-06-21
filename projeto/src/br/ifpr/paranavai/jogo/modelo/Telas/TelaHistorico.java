5f9d4e7c6984a8cca4c66b83101e9248db170e6a
unknown <joaomarcosp.sza@gmail.com>
2023-06-21 17:06:08 -0300

    Pequena alteração - V.65.1

--- projeto/src/br/ifpr/paranavai/jogo/modelo/Telas/TelaHistorico.java
+++ projeto/src/br/ifpr/paranavai/jogo/modelo/Telas/TelaHistorico.java
@@ -1,8 +1,11 @@
 package br.ifpr.paranavai.jogo.modelo.Telas;
 
 import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 import java.awt.Graphics;
 import javax.swing.ImageIcon;
+import java.awt.*;
 
 public class TelaHistorico {
     private Image imagem_historico;
@@ -10,19 +13,51 @@ public class TelaHistorico {
     private int larguraImagem, alturaImagem;
     private boolean historico_visibilidade;
 
+    private Font pixel = null;
+
     public TelaHistorico() {
+        try {
+            // CARREGA A FONTE A PARTIR DO ARQUIVO
+            pixel = Font.createFont(Font.TRUETYPE_FONT, new File("recursos\\fontes\\pixel_fonte.ttf"));
+            // DEFINE O TAMANHO DA FONTE DESEJADO
+
+        } catch (IOException | FontFormatException e) {
+            e.printStackTrace();
+        }
         this.historico_visibilidade = true;
     }
 
     public void carregar() {
         ImageIcon carregando = new ImageIcon("recursos\\sprites_fundos\\fundo_menu.jpg");
         this.imagem_historico = carregando.getImage();
-        this.larguraImagem = this.imagem_historico.getWidth(null);
-        this.alturaImagem = this.imagem_historico.getHeight(null);
+        this.alturaImagem = this.imagem_historico.getWidth(null);
+        this.larguraImagem = this.imagem_historico.getHeight(null);
     }
 
     public void titulo_Historico(Graphics g) {
         g.drawImage(imagem_historico, 0, 0, null);
+
+        String titulo = "HISTORICO";
+        Font fonte = pixel;
+
+        fonte = fonte.deriveFont(Font.BOLD, 85);
+
+        g.setColor(new Color(255, 209, 70)); // COR DO TITULO AMARELO
+        g.setFont(fonte);
+        // CRENTALIZA VERTICALMENTE
+        FontMetrics fm = g.getFontMetrics();
+
+        int tituloWidth = fm.stringWidth(titulo); // PEGA O TAMANHO DA STRING
+        int x = (alturaImagem - tituloWidth) / 2;
+        int y = 100 + (larguraImagem - fm.getHeight()) / 2;
+        //
+        g.setColor(Color.WHITE); // DEFINE A COR PRINCIPAL DO TÍTULO - BRANCO
+        g.drawString(titulo, (x + 7), y - 280); // POSIÇÃO DO TITULO - AMARELO
+        // SUBTITULO COR AMARELO
+        // DESENHA O SUBTÍTULO NA TELA - AMARELO
+        // t.setColor(new Color(255, 209, 70));
+        g.setColor(new Color(255, 209, 70));
+        g.drawString(titulo, x, y - 280); // POSIÇÃO DO TÍTULO PRINCIPAL - COR BRANCA
     }
 
     public boolean isHistorico_visibilidade() {
@@ -72,7 +107,5 @@ public class TelaHistorico {
     public void setAlturaImagem(int alturaImagem) {
         this.alturaImagem = alturaImagem;
     }
-
-    
 
 }
--- projeto/src/br/ifpr/paranavai/principal/Principal.java
+++ projeto/src/br/ifpr/paranavai/principal/Principal.java
@@ -1,7 +1,6 @@
 package br.ifpr.paranavai.principal;
 
 import javax.swing.JFrame;
-
 import br.ifpr.paranavai.jogo.modelo.Fase;
 
 public class Principal extends JFrame {
@@ -11,7 +10,7 @@ public class Principal extends JFrame {
         super.add(fase);
 
         super.setTitle("Star Wars: Invader");
-        super.setSize(1300, 760);
+        super.setSize(1300, 700);
         super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         super.setLocationRelativeTo(null);
         super.setResizable(false);
