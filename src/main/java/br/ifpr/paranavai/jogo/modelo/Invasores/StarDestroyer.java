9d3075edb6519649c86901ef6838baf3763d89f6
joaomarcosp-souza <joaomarcosp.sza@gmail.com>
2023-09-25 20:08:36 -0300

    Adicionando Maven ao projeto v1.18.1

---
+++ src/main/java/br/ifpr/paranavai/jogo/modelo/Invasores/StarDestroyer.java
@@ -0,0 +1,39 @@
+package br.ifpr.paranavai.jogo.modelo.Invasores;
+
+import javax.swing.ImageIcon;
+import br.ifpr.paranavai.jogo.modelo.Base;
+
+public class StarDestroyer extends Base {
+
+    private static final String PATH_IMAGE = "src/main/resources/Sprites/Inimigos/starDestroyer.png";
+
+    public StarDestroyer(int posicaoEmX, int posicaoEmY) {
+        super.setInitialSpeed(3);
+        super.setSpeed(super.getInitialSpeed());
+        super.setPositionInX(posicaoEmX);
+        super.setPositionInY(posicaoEmY);
+        super.setVisibility(true);
+    }
+
+    @Override
+    public void carregar() {
+        ImageIcon loading = new ImageIcon(PATH_IMAGE);
+        super.setImage(loading.getImage());
+        super.setWidthImage(super.getImage().getWidth(null));
+        super.setHeightImage(super.getImage().getHeight(null));
+    }
+
+    @Override
+    public void atualizar() {
+        super.setPositionInX((int) (super.getPositionInX() - super.getSpeed()));
+        if (super.getPositionInX() < -1000) {
+            super.setVisibility(false);
+        }
+
+        if (super.getPositionInY() < 0) {
+            super.setPositionInY(0);
+        } else if (super.getPositionInY() > super.getScreenResolution().HEIGHT_SCREEN - super.getHeightImage()) {
+            super.setPositionInY(super.getScreenResolution().HEIGHT_SCREEN - super.getHeightImage());
+        }
+    }
+}
---
+++ src/main/java/br/ifpr/paranavai/jogo/modelo/Telas/Controles.java
@@ -0,0 +1,39 @@
+package br.ifpr.paranavai.jogo.modelo.Telas;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+public class Controles extends TelasBase {
+    
+    private Image imageControl;
+    // CAMINHO IMAGEM DE FUNDO
+    private static final String PATH_IMAGE = "src/main/resources/Sprites/Fundos/FundoTelas.jpg";
+    private static final String PATH_IMAGE_TITLE = "src/main/resources/Sprites/Fundos/TelaControle.png";
+    
+    public Controles() {
+        super.setVisibility(false);
+    }
+
+    @Override
+    public void carregar() {
+        ImageIcon loading = new ImageIcon(PATH_IMAGE);
+        super.setImage(loading.getImage());
+        super.setImage(super.getImage().getScaledInstance(getScreenResolution().WIDTH_SCREEN, getScreenResolution().HEIGHT_SCREEN,
+                Image.SCALE_FAST));
+
+        // IMAGEM SOBREPOSIÇÃO
+        ImageIcon loadingImageControl = new ImageIcon(PATH_IMAGE_TITLE);
+        this.imageControl = loadingImageControl.getImage();
+        // REDIMENSIONA O TAMANHO DA IMAGEM PARA O TAMANHO DA TELA
+        this.imageControl = this.imageControl.getScaledInstance(getScreenResolution().WIDTH_SCREEN,
+                getScreenResolution().HEIGHT_SCREEN,
+                Image.SCALE_FAST);
+    };
+
+    // CARREGANDO A IMAGEM DE FUNDO E A IMAGEM COM OS COMANDOS
+    public void conteudo(Graphics g) {
+        g.drawImage(super.getImage(), 0, 0, null);
+        g.drawImage(imageControl, 0, 0, null);
+    }
+}
